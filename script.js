546e89e45f1cba5a4524e8053ed55712e4a03155
kenproxx <kienhue98@gmail.com>
2022-12-09 21:13:36 +0700

    init

---
+++ script.js
@@ -0,0 +1,30 @@
+let slideShow = document.getElementById('slideShow');
+let language = false
+function show() {
+  slideShow.innerHTML = `            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src="./img/anh1.jpg" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="./img/anh2.jpg" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="./img/anh3.jpg" alt="Third slide">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+`
+}
+
+function hide() {
+  slideShow.innerHTML = '';
+}
